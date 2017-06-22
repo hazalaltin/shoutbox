821683cfc333674b57ca37a3ba4e759246efa104
hazalaltin <d13124676@mydit.ie>
2017-06-22 15:09:22 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,23 @@
+ <?php
+  
+      include "database.php";
+	  ?>
+ 
+ <?php
+ //create select query
+ $query = "SELECT * FROM shouts";
+ $shouts = mysqli_query($con,$query);
+ 
+ ?>
+ 
+ 
  <!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8">
     <title> Shout it </title>
     
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="style.css" type="text/css" />
   </head>
   <body>
   
@@ -14,26 +27,30 @@
   </header>
   
   <div id="shouts">
-<ul>
-  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
-  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
-  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
-    <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
-      <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
-        <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+      <ul>
+
+<?php while($row = mysqli_fetch_assoc ($shouts)) : ?>
+ 
+ <li class="shout"><span> <?php echo $row ['time'] ?> </span><?php echo $row ['user'] ?>  : <?php echo $row ['message'] ?></li>
+ 
+<?php endwhile; ?>    
         </ul>
 </div>
   
   <div id="input"> 
+  <?php if(isset($_GET['error'])) : ?>
+  <div class="error"> <?php echo $_GET['error']; ?></div>
+  <?php endif; ?>
+  
   <form method="post" action="process.php">
   <input type="text"  name = "user" placeholder="enter your name"/>
   <input type="text"  name = "message" placeholder="enter your message"/>  
   <br/>
   <input class="shout-btn" type="submit" name="submit" value="shout it out";
-  
-  
-  
+  </form>
   </div>
+  </div>
+  
   
   
   </body>
