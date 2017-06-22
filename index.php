f7e0a79128515f706043b81d0f977d1ccf2bfd7b
hazalaltin <d13124676@mydit.ie>
2017-06-22 10:11:10 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,40 @@
+ <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title> Shout it </title>
+    
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+  </head>
+  <body>
+  
+  <div id="container">
+  <header>
+  <h1> SHOUT IT! ShoutBox</h1>
+  </header>
+  
+  <div id="shouts">
+<ul>
+  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+  <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+    <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+      <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+        <li class="shout"><span> 10.15 - </span> Brad : Hey what are tyou up to </li>
+        </ul>
+</div>
+  
+  <div id="input"> 
+  <form method="post" action="process.php">
+  <input type="text"  name = "user" placeholder="enter your name"/>
+  <input type="text"  name = "message" placeholder="enter your message"/>  
+  <br/>
+  <input class="shout-btn" type="submit" name="submit" value="shout it out";
+  
+  
+  
+  </div>
+  
+  
+  </body>
+</html>
